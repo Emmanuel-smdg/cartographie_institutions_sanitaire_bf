de8aa31a0a8c4a83b0bc7a566c1cc0dc11a69fb0
lariseve <larissa0182002@gmail.com>
2024-09-18 10:13:34 +0000

    changer unp la carte

--- js/carte.js
+++ js/carte.js
@@ -1,225 +1,90 @@
-// Centrer la carte sur le Burkina.
-let map = L.map("map").fitBounds([
-  [15.083, -5.478], // Nord-Ouest du Burkina Faso
-  [9.391, 2.406], // Sud-Est du Burkina Faso
-]);
+let map = L.map("map").setView([12.0, -1.0], 12); 
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 20,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map); //Affiche la carte
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map); 
 
-// Ce layer est mis pour permettre de mieux voir les contours du pays et de ses régions dans la carte.
-const regionbfLayer = L.geoJSON(regionbf, {
-  style(feature) {
-    return {
-      color: "#00008B", // Couleur des bordures
-      weight: 2, // Épaisseur des bordures
-      opacity: 1,
-      fillOpacity: 0,
-    };
-  },
-}).addTo(map);
-
-// Fonction pour créer les icones personnalisées
-function createCustomIcon(
-  iconUrl,
-  iconSize = [32, 32],
-  iconAnchor = [16, 32],
-  popupAnchor = [0, -32]
-) {
+function createCustomIcon(iconUrl, iconSize = [24, 24], iconAnchor = [12, 24], popupAnchor = [0, -24]) {
   return L.icon({
-    iconUrl: iconUrl, // Chemin vers l'image de l'icône
-    iconSize: iconSize, // Taille de l'icône [width, height]
-    iconAnchor: iconAnchor, // Point d'ancrage [x, y]
-    popupAnchor: popupAnchor, // Ancrage du popup [x, y]
+    iconUrl: iconUrl,
+    iconSize: iconSize,
+    iconAnchor: iconAnchor,
+    popupAnchor: popupAnchor,
   });
 }
-// Création des icônes personnalisées pour les CHR et CHU
-let chrIcon = createCustomIcon("lib/images/chr.png");
-let chuIcon = createCustomIcon("lib/images/chu.png");
-let labIcon = createCustomIcon("lib/images/laboratoire.png");
-let depotIcon = createCustomIcon("lib/images/trousse.png");
-let hopitalIcon = createCustomIcon("lib/images/hopital.png");
-let clininqueIcon = createCustomIcon("lib/images/clinique.png");
-let cmaIcon = createCustomIcon("lib/images/cma.png");
-let cspsIcon = createCustomIcon("lib/images/csps.png");
 
-// Fonction pour créer un GeoJSON Layer en fonction du type et de l'icône
+let icons = {
+  "CHU": createCustomIcon("lib/images/chu.png"),
+  "CHR": createCustomIcon("lib/images/chr.png"),
+  "Laboratoire": createCustomIcon("lib/images/laboratoire.png"),
+  "Dépôt": createCustomIcon("lib/images/trousse.png"),
+  "Hôpital": createCustomIcon("lib/images/hopital.png"),
+  "Clinique": createCustomIcon("lib/images/clinique.png"),
+  "CMA": createCustomIcon("lib/images/cma.png"),
+  "CSPS": createCustomIcon("lib/images/csps.png"),
+};
+
 function createLayer(data, icon) {
   return L.geoJSON(data, {
     pointToLayer: function (feature, latlng) {
       return L.marker(latlng, { icon: icon });
     },
     onEachFeature: function (feature, layer) {
-      // Définir le chemin de l'image (image fournie ou par défaut selon la catégorie)
-      let imageSrc = feature.properties.image
-        ? feature.properties.image
-        : getDefaultImagePath(feature.properties.catégorie);
-
-      // Construction du contenu du popup avec l'image en première position
-      let popupContent =
-        "<div class='popup-content mb-2'>" +
-        "<img src='" +
-        imageSrc +
-        "' class='popup-image' alt='Image'><br>" +
-        "<h5>" +
-        feature.properties.nom +
-        "</h5><br>" +
-        "Localité: " +
-        feature.properties.localité +
-        "<br>" +
-        "Catégorie: " +
-        feature.properties.catégorie +
-        "<br>" +
-        "Téléphone: " +
-        feature.properties.téléphone +
-        "<br>" +
-        "Adresse: " +
-        feature.properties.adresse;
-      if (feature.properties.nom_du_responsable) {
-        popupContent +=
-          "<br>Nom du Responsable: " + feature.properties.nom_du_responsable;
-      }
-      if (feature.properties.poste_du_responsable) {
-        popupContent += "<br>Poste: " + feature.properties.poste_du_responsable;
-      }
-
-      popupContent += "</div>";
+      let popupContent = `<div class='popup-content mb-2'>
+                            <h5>${feature.properties.nom}</h5>
+                            Localité: ${feature.properties.localité}<br>
+                            Catégorie: ${feature.properties.catégorie}<br>
+                            Téléphone: ${feature.properties.téléphone}<br>
+                            Email: ${feature.properties.email}<br>
+                            Site Web: ${feature.properties.site_web}<br>
+                            Adresse: ${feature.properties.adresse}
+                          </div>`;
       layer.bindPopup(popupContent);
     },
   });
 }
 
-// Création des Couches
-let chrLayer = createLayer(chr, chrIcon);
-let chuLayer = createLayer(chu, chuIcon);
-let labLayer = createLayer(lab, labIcon);
-let depotLayer = createLayer(depot, depotIcon);
-let hopitalLayer = createLayer(hopital, hopitalIcon);
-let cliniqueLayer = createLayer(clinique, clininqueIcon);
-let cmaLayer = createLayer(cma, cmaIcon);
-let cspsLayer = createLayer(csps, cspsIcon);
-
-// Affichage des différentes couches de l'application
-let overlayMaps = {
-  "Centres Hospitaliers Universitaires (CHU)": chuLayer,
-  "Centres Hospitaliers Régionaux (CHR)": chrLayer,
-  "Laboratoires d'analyses médicales": labLayer,
-  "Dépôts pharmaceutique": depotLayer,
-  Hopitaux: hopitalLayer,
-  Cliniques: cliniqueLayer,
-  "Centres Médicaux avec Antenne chirurgicale (CMA)": cmaLayer,
-  "Centres de Santé et de Promotion Sociale (CSPS)": cspsLayer,
+let layers = {
+  "CHU": createLayer(chu, icons["CHU"]),
+  "CHR": createLayer(chr, icons["CHR"]),
+  "Laboratoire": createLayer(lab, icons["Laboratoire"]),
+  "Dépôt": createLayer(depot, icons["Dépôt"]),
+  "Hôpital": createLayer(hopital, icons["Hôpital"]),
+  "Clinique": createLayer(clinique, icons["Clinique"]),
+  "CMA": createLayer(cma, icons["CMA"]),
+  "CSPS": createLayer(csps, icons["CSPS"]),
 };
 
-// Ajout du contrôle de couches
-L.control.layers(null, overlayMaps).addTo(map);
+function setDefaultLayers() {
+  layers["CHR"].addTo(map);
+  layers["CMA"].addTo(map);
+}
 
-// Afficher toutes les couches par défaut
-chrLayer.addTo(map);
-chuLayer.addTo(map);
-labLayer.addTo(map);
-depotLayer.addTo(map);
-hopitalLayer.addTo(map);
-cliniqueLayer.addTo(map);
-cmaLayer.addTo(map);
-cspsLayer.addTo(map);
+let overlayMaps = {};
+Object.keys(layers).forEach(function (key) {
+  overlayMaps[key] = layers[key];
+});
 
-// Ajout de la recherche sur la carte
+L.control.layers(null, overlayMaps, { collapsed: false }).addTo(map);
+
+setDefaultLayers(); 
+
 let searchControl = new L.Control.Search({
-  layer: L.layerGroup([
-    chrLayer,
-    chuLayer,
-    labLayer,
-    depotLayer,
-    hopitalLayer,
-    cliniqueLayer,
-    cmaLayer,
-    cspsLayer,
-  ]),
-  propertyName: "nom", // Recherche par le nom des hôpitaux
+  layer: L.layerGroup(Object.values(layers)),
+  propertyName: "nom", 
   marker: false,
   initial: false,
   moveToLocation: function (latlng, title, map) {
-    // Centrer la carte sur l'élément recherché
-    map.setView(latlng, 15); // Zoom sur l'élément recherché
+    map.setView(latlng, 15); 
   },
 }).addTo(map);
 
-// Ajout d'une légende personnalisée
-let legend = L.control({ position: "bottomleft" });
-
-legend.onAdd = function (map) {
-  let div = L.DomUtil.create("div", "bg-light rounded p-1"),
-    labels = ["<strong>Légende :</strong>"],
-    categories = [
-      "CHU",
-      "CHR",
-      "LABO",
-      "DEPOT",
-      "HOPITAL",
-      "CLINIQUE",
-      "CMA",
-      "CSPS",
-    ];
-
-  let icons = {
-    CHU: "lib/images/chu.png",
-    CHR: "lib/images/chr.png",
-    LABO: "lib/images/laboratoire.png",
-    DEPOT: "lib/images/trousse.png",
-    HOPITAL: "lib/images/hopital.png",
-    CLINIQUE: "lib/images/clinique.png",
-    CMA: "lib/images/cma.png",
-    CSPS: "lib/images/csps.png",
-  };
-
-  categories.forEach(function (category) {
-    div.innerHTML += labels.push(
-      '<img src="' +
-        icons[category] +
-        '" style="width:24px;height:24px; margin-bottom:0.25rem"> ' +
-        category
-    );
-  });
-
-  div.innerHTML = labels.join("<br>");
-  return div;
-};
-
-legend.addTo(map);
-
-// Fonction pour définir le chemin de l'image par défaut selon la catégorie
-function getDefaultImagePath(categorie) {
-  switch (categorie) {
-    case "CENTRE DE SANTE PUBLIQUE":
-      return "lib/images/cma.png";
-    case "Centres de Santé et de Promotion Sociale":
-      return "lib/images/csps.png";
-    case "Centre Hospitalier Régional":
-      return "lib/images/chr.png";
-    case "Centre Hospitalier Universitaire":
-      return "lib/images/chu.png";
-    case "Laboratoire d'analyses médicales":
-      return "lib/images/laboratoire.png";
-    case "Dépôt pharmaceutique":
-      return "lib/images/trousse.png";
-    case "Hôpital privé":
-      return "lib/images/hopital.png";
-    case "Clinique":
-      return "lib/images/clinique.png";
-    default:
-      return "lib/images/csps.png";
-  }
-}
-
-// Ajout de la bibliothèque Leaflet Routing Machine
 let routingControl;
 
 function calculateRoute(start, end) {
-  if (routingControl) {
+  if
+  (routingControl) {
     map.removeControl(routingControl);
   }
 
@@ -239,37 +104,27 @@ function calculateRoute(start, end) {
   routingControl.on("routesfound", function (e) {
     let routes = e.routes;
     let summary = routes[0].summary;
-    // Temps en secondes, distance en mètres
-    let popupContent = `Distance: ${(summary.totalDistance / 1000).toFixed(
-      2
-    )} km<br>
+    let popupContent = `Distance: ${(summary.totalDistance / 1000).toFixed(2)} km<br>
                         Durée: ${Math.round(summary.totalTime / 60)} minutes`;
     L.popup().setLatLng(end).setContent(popupContent).openOn(map);
   });
 }
 
-// Fonctionnalité affichage et calcul de l'itinéraire depuis ma position
 map.on("popupopen", function (e) {
   let marker = e.popup._source;
   if (marker) {
     let popupContent = e.popup.getContent();
-    if (typeof popupContent === "string") {
-      popupContent = `<div>${popupContent}</div>`;
-    }
     let container = L.DomUtil.create("div", "");
     container.innerHTML = popupContent;
 
-    let startBtn = L.DomUtil.create("button", "center btn btn-info", container);
+    let startBtn = L.DomUtil.create("button", "btn btn-info", container);
     startBtn.innerHTML = "Calculer l'itinéraire depuis ma position";
     startBtn.style.cursor = "pointer";
 
     L.DomEvent.on(startBtn, "click", function () {
       if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function (position) {
-          let start = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+          let start = { lat: position.coords.latitude, lng: position.coords.longitude };
           let end = marker.getLatLng();
           calculateRoute(start, end);
         });
@@ -282,7 +137,6 @@ map.on("popupopen", function (e) {
   }
 });
 
-// Ajout d'un contrôle pour effacer l'itinéraire
 L.Control.ClearRoute = L.Control.extend({
   onAdd: function (map) {
     let btn = L.DomUtil.create("button", "btn btn-secondary text-center mt-2");
@@ -298,3 +152,19 @@ L.Control.ClearRoute = L.Control.extend({
 });
 
 new L.Control.ClearRoute({ position: "topright" }).addTo(map);
+
+let legend = L.control({ position: "bottomleft" });
+
+legend.onAdd = function (map) {
+  let div = L.DomUtil.create("div", "bg-light rounded p-1"),
+    labels = ["<strong>Légende :</strong>"],
+    categories = Object.keys(icons);
+
+  categories.forEach(function (category) {
+    div.innerHTML += `<img src="${icons[category].options.iconUrl}" style="width:24px;height:24px; margin-bottom:0.25rem"> ${category}<br>`;
+  });
+
+  return div;
+};
+
+legend.addTo(map);
